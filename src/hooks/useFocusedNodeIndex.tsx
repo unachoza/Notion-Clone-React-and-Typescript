4eaf8db0c12a41364d65174448fdeaf088d17fc5
arianna choza <unachoza@gmail.com>
2024-04-05 12:34:34 -0400

    fixed focusedNodeIndex hook

---
+++ src/hooks/useFocusedNodeIndex.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState, Dispatch, SetStateAction } from "react";
+import { NodeData } from "../utils/types";
+
+type useFocusedNodeIndexProps = {
+	nodes: NodeData[];
+};
+
+export const useFocusedNodeIndex = ({ nodes }: useFocusedNodeIndexProps): [number, Dispatch<SetStateAction<number>>] => {
+	const [focusedNodeIndex, setFocusedNodeIndex] = useState(0);
+
+	useEffect(() => {
+		const onKeyDown = (event: KeyboardEvent) => {
+			if (event.key === "ArrowUp") {
+				setFocusedNodeIndex((index) => Math.max(index - 1, 0));
+			}
+			if (event.key === "ArrowDown") {
+				setFocusedNodeIndex((index) => Math.min(index + 1, nodes.length - 1));
+			}
+		};
+		document.addEventListener("keydown", onKeyDown);
+
+		return () => document.removeEventListener("keydown", onKeyDown);
+	}, [nodes]);
+
+	return [focusedNodeIndex, setFocusedNodeIndex];
+};
