3135efa9aa988f5b7af8f6ff2b177e7824b87c99
arianna choza <unachoza@gmail.com>
2024-04-10 17:33:30 -0400

    added dnd library and drag and drop functionality

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -2,10 +2,12 @@ import { useFocusedNodeIndex } from "../hooks/useFocusedNodeIndex";
 import Cover from "./Cover/Cover";
 import Spacer from "./Spacer/Spacer";
 import Title from "./Title/Title";
-import BasicNode from "../Node/BasicNode/BasicNode";
 import { nanoid } from "nanoid";
 import { useAppState } from "../context/AppStateContext";
 import { NodeData } from "../utils/types";
+import { DndContext, DragOverlay, DragEndEvent } from "@dnd-kit/core";
+import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import NodeContainer from "../Node/NodeContainer/NodeContainer";
 
 type PageNodeProps = {
 	node: NodeData;
@@ -14,23 +16,35 @@ type PageNodeProps = {
 };
 
 export const Page = () => {
-	const { nodes, addNode, title, setTitle } = useAppState();
+	const { nodes, addNode, title, setTitle, reorderNodes } = useAppState();
 	const [focusedNodeIndex, setFocusedNodeIndex] = useFocusedNodeIndex({ nodes });
+
+	const handleDragEnd = (event: DragEndEvent) => {
+		const { active, over } = event;
+		if (over?.id && active.id !== over?.id) {
+			reorderNodes(active.id as string, over.id as string);
+		}
+	};
 
 	return (
 		<>
 			<Cover />
 			<div>
 				<Title addNode={addNode} title={title} changePageTitle={setTitle} />
-				{nodes.map((node, index) => (
-					<BasicNode
-						key={node.id}
-						node={node}
-						isFocused={focusedNodeIndex === index}
-						updateFocusedIndex={setFocusedNodeIndex}
-						index={index}
-					/>
-				))}
+				<DndContext onDragEnd={handleDragEnd}>
+					<SortableContext items={nodes} strategy={verticalListSortingStrategy}>
+						{nodes.map((node, index) => (
+							<NodeContainer
+								key={node.id}
+								node={node}
+								isFocused={focusedNodeIndex === index}
+								updateFocusedIndex={setFocusedNodeIndex}
+								index={index}
+							/>
+						))}
+					</SortableContext>
+					<DragOverlay />
+				</DndContext>
 				<Spacer
 					handleClick={() => {
 						addNode({ type: "text", value: "", id: nanoid() }, nodes.length);
